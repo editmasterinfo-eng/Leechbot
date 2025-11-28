50080e3613e613aa102e381ebdcecb98700681bf
editmasterinfo-eng <editmasterinfo@gmail.com>
2025-11-28 19:44:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "api id"
-api_hash = "api hadh"
-bot_token = "bot token"
+api_id = "22177421"
+api_hash = "e515bbf4a302d7c7335f689a52b196a5"
+bot_token = "8292830889:AAGeQJ9x3T428f4cr3Pxkd2kQoL87CnG2og"
 auth_users = [7890781002]
 sudo_users = [7890781002]
