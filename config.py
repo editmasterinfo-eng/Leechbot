bbcb4760a0bb49d807737be0a5f6e5d2354e0fd5
editmasterinfo-eng <editmasterinfo@gmail.com>
2025-11-29 18:11:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
-api_id = "22177421"
+api_id = 22177421
 api_hash = "e515bbf4a302d7c7335f689a52b196a5"
 bot_token = "8292830889:AAGeQJ9x3T428f4cr3Pxkd2kQoL87CnG2og"
+
 auth_users = [1865244712]
 sudo_users = [1865244712]
+
+# Apne private channel ka ID yahan daalein. Yeh -100 se shuru hoga.
+INTRO_CHANNEL_ID = -1003477496967
